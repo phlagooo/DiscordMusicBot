19cd2870a1e2b04cd7cfd35672ce9d927b4bb9d5
phlagooo <104774758+phlagooo@users.noreply.github.com>
2024-09-24 20:56:54 +0200

    Removed confusing UX.

--- music_player.py
+++ music_player.py
@@ -2,7 +2,6 @@ class Music_player():
     def __init__(self):
         self.queue = []
         self.loop = False
-        self.previous_song = None
         self.current_song = None
 
     def add_song(self, song):
@@ -17,13 +16,9 @@ class Music_player():
     def get_next_song(self):
         next_song = self.queue[0]
         self.queue = self.queue[1:]
-        # Store previous song
-        self.previous_song = self.current_song
+        # dont Store previous song
         self.current_song = next_song
         return next_song
-    
-    def get_previous_song(self):
-        return self.previous_song
     
     def get_current_song(self):
         return self.current_song
@@ -33,7 +28,3 @@ class Music_player():
 
     def toggle_loop(self):
         self.loop = not self.loop
-        self.previous_song = self.current_song
-    
-    def is_current_and_previous_same(self):
-        return self.previous_song == self.current_song != None
